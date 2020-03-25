e13e25c535cdd93e621b94a1afed0b8d5b7b1e54
dantsub <dany.mrtinez@gmail.com>
2020-03-25 20:26:51 +0000

    add task

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# install and configure an Nginx server using Puppet instead of Bash
+
+package { 'nginx':
+  ensure  => installed,
+  name    => 'nginx',
+}
+
+file { '/var/www/html/index.html':
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School',
+}
+
+file_line { 'title':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'location /redirect_me {\n\treturn 301 https://www.holbertonschool.com/co;\n}',
+  multiple => true,
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}

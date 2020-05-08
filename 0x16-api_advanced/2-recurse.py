e0e531c607152e49401efb888f10caf2041a2ac2
dantsub <dany.mrtinez@gmail.com>
2020-05-07 19:43:25 -0500

    task 2 done

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
+"""Module recurse
+Returns a list containing the titles
+of all hot articles for a given subreddit.
 """
-"""
-from requests import request
+from requests import *
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    """"""
+    """ Function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit.
+    If no results are found for the given subreddit, the function should
+    return None.
+    """
+    # ===================== Request ===========================
+    api = 'https://api.reddit.com/r/{}/hot?after={}'.format(subreddit, after)
+    headers = {'User-Agent': 'Holbi'}
+    response = get(api, headers=headers).json()
+    # =========================================================
+    try:
+        for top in response.get('data')['children']:
+            hot_list.append(top['data']['title'])
+        if response['data']['after'] is not None:
+            recurse(subreddit, hot_list, response['data']['after'])
+        return hot_list
+    except Exception:
+        return None

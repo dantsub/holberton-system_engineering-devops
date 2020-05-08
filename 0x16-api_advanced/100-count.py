29b2e628a6aa018bc70c16f788fce5f476ac3f61
dantsub <dany.mrtinez@gmail.com>
2020-05-07 23:55:16 -0500

    try 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,8 +1,7 @@
-
 #!/usr/bin/python3
 """A file to make a query to an endpoint
 """
-from requests import *
+from requests import request
 
 
 def count_words(subreddit, word_list, after="", counter={}, ini=0):
@@ -13,24 +12,22 @@ def count_words(subreddit, word_list, after="", counter={}, ini=0):
     if ini == 0:
         for word in word_list:
             counter[word] = 0
-
     url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
     headers = {"User-Agent": "Python3"}
-    response = get(url, headers=headers).json()
-
+    response = request("GET", url, headers=headers).json()
     try:
+        top = response['data']['children']
         _after = response['data']['after']
-        for item in response['data']['children']:
+        for item in top:
             for word in counter:
                 counter[word] += item['data']['title'].lower(
                     ).split(' ').count(word.lower())
-        if response['data']['after']:
+        if _after is not None:
             count_words(subreddit, word_list, _after, counter, 1)
         else:
-            letters = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
-            for name, num in letters:
+            str = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
+            for name, num in str:
                 if num != 0:
                     print('{}: {}'.format(name, num))
-
     except Exception:
-        return
+        return None

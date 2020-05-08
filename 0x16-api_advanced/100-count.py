1a05b95f5648d933453eb974420b76bcbc97ad01
dantsub <dany.mrtinez@gmail.com>
2020-05-08 00:13:56 -0500

    like before

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,7 +1,8 @@
+
 #!/usr/bin/python3
 """A file to make a query to an endpoint
 """
-from requests import *
+from requests import request
 
 
 def count_words(subreddit, word_list, after="", counter={}, ini=0):
@@ -12,28 +13,22 @@ def count_words(subreddit, word_list, after="", counter={}, ini=0):
     if ini == 0:
         for word in word_list:
             counter[word] = 0
-
     url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
     headers = {"User-Agent": "Python3"}
-    response = get(url, headers=headers).json()
-
+    response = request("GET", url, headers=headers).json()
     try:
+        top = response['data']['children']
         _after = response['data']['after']
-        for item in response['data']['children']:
+        for item in top:
             for word in counter:
-                if counter[word]:
-                    counter[word] += item['data']['title'].lower(
-                        ).split(' ').count(word.lower())
-                else:
-                    counter[word] = item['data']['title'].lower(
-                        ).split(' ').count(word.lower())
-        if response['data']['after']:
+                counter[word] += item['data']['title'].lower(
+                    ).split(' ').count(word.lower())
+        if _after is not None:
             count_words(subreddit, word_list, _after, counter, 1)
         else:
-            letters = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
-            for name, num in letters:
+            str = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
+            for name, num in str:
                 if num != 0:
                     print('{}: {}'.format(name, num))
-
     except Exception:
-        return
+        return None
